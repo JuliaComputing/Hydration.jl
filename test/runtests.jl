57b85f7f7caaa9a93f3b637ac19df623da250455
Frames White <me@oxinabox.net>
2023-06-19 14:05:44 +0800

    Intial version

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using Hydration
 using Test
+using Dates
 
-@testset "Hydration.jl" begin
-    # Write your tests here.
+@testset "Hydration.jl" begin 
+    @testset "$file" for file in (
+        "timing_utils.jl", 
+        "water.jl",
+    )
+        include(file)
+    end
 end
